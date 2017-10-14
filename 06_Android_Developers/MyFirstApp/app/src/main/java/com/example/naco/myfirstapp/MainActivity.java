3fa3815193fdf521f50072621e89209fdc9a62c1
nguyenhoangvannha <1512363@student.hcmus.edu.vn>
2017-10-14 23:49:17 +0700

    Start learn dev.android.com

---
+++ 06_Android_Developers/MyFirstApp/app/src/main/java/com/example/naco/myfirstapp/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.naco.myfirstapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    EditText editText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText = (EditText) findViewById(R.id.edit_message);
+    }
+
+    //Calles when user clicks the Send button
+    public void sendMessage(View v){
+        //Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
